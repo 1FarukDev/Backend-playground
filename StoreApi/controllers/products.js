ad20a942ff56770f32dec195f41b2c7d0dc4d3d3
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-09-01 01:31:13 +0100

    Feat: Done with the filtering

--- StoreApi/controllers/products.js
+++ StoreApi/controllers/products.js
@@ -5,8 +5,19 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const products = await Products.find()
-    res.status(200).json({ msg: products, count: products.length })
+    const { featured, company, name } = req.query
+    const queryObject = {}
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+    const products = await Products.find(queryObject)
+    res.status(200).json({ products, count: products.length })
 }
 
 module.exports = {
