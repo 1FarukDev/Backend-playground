e1ebba4cfaf3c8e6f1775c83e0c50b07408b33d0
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-09-03 00:51:02 +0100

    Feat:Done with the fields(selecting field to show)

--- StoreApi/controllers/products.js
+++ StoreApi/controllers/products.js
@@ -5,7 +5,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
@@ -24,6 +24,11 @@ const getAllProducts = async (req, res) => {
     } else {
         result = result.sort('createdAt')
     }
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+
     const products = await result
     res.status(200).json({ products, count: products.length })
 }
