3fd89b6a42c3fda4c14d5579928314664bbc8a13
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-10-04 09:39:09 +0100

    feat: done with the jobapi

--- JobAPI/controllers/jobs.js
+++ JobAPI/controllers/jobs.js
@@ -4,27 +4,57 @@ const { BadRequestError, NotFoundError } = require('../errors')
 
 
 const getAllJobs = async (req, res) => {
-    const jobs = await Job.find({ createBy: req.user.userId }).sort('createdAt')
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
     res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
-const getJob = async (req, res) => {
-    res.send('get job')
-}
-const createJpb = async (req, res) => {
+
+const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId
     const job = await Job.create(req.body)
     res.status(StatusCodes.CREATED).json({ job })
 }
+const getJob = async (req, res) => {
+    const { user: { userId }, params: { id: jobId } } = req
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId
+    })
+    if (!job) {
+        throw new NotFoundError(`No job with ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
+}
 const updateJob = async (req, res) => {
-    res.send('update job')
+    const {
+        user: { userId },
+        params: { id: jobId },
+        body: { company, position }
+    } = req
+
+    if (company === '' || position === '') {
+        throw new BadRequestError('Compnay or Position fields cannot be empty')
+    }
+    const job = await Job.findByIdAndUpdate({ _id: jobId, createdBy: userId }, req.body, { new: true, runValidalitors: true })
+    if (!job) {
+        throw new NotFoundError(`No job with ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 const deleteJob = async (req, res) => {
-    res.send('Delete job')
+    const { user: { userId }, params: { id: jobId } } = req
+    const job = await Job.findByIdAndDelete({
+        _id: jobId,
+        createdBy: userId
+    })
+    if (!job) {
+        throw new NotFoundError(`No job with ${jobId}`)
+    }
+    res.status(StatusCodes.OK).send({ msg: "Job has been successfully deleted" })
 }
 
 module.exports = {
     getAllJobs,
-    createJpb,
+    createJob,
     updateJob,
     deleteJob,
     getJob
--- JobAPI/routes/jobs.js
+++ JobAPI/routes/jobs.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const { getAllJobs, getJob, createJpb, updateJob, deleteJob } = require('../controllers/jobs')
+const { getAllJobs, getJob, createJob, updateJob, deleteJob } = require('../controllers/jobs')
 
-router.route('/').post(createJpb).get(getAllJobs)
+router.route('/').post(createJob).get(getAllJobs)
 router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
 
 
