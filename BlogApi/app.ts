504ac79614134f4f9ade71919d709b0c7aac22ab
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-10-05 18:08:16 +0100

    Feat: Added needed dependencies

--- BlogApi/app.ts
+++ BlogApi/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const blogsRouter = require('./routes/blog')
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript with Express!');
