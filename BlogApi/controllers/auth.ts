ba069c0afca6ef419403e872444b83d6670b68ca
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-10-05 20:16:55 +0100

    Feat: Done with the user registration

--- BlogApi/controllers/auth.ts
+++ BlogApi/controllers/auth.ts
@@ -1,8 +1,20 @@
 import express, { Request, Response } from 'express';
+import { BadRequestError } from '../errors';
+import User from '../models/User'
+import { StatusCodes } from 'http-status-codes';
+
+
+
 const register = async (req: Request, res: Response) => {
-    res.send('register user')
+    const user = await User.create({ ...req.body })
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        throw new BadRequestError('Please Provide email and password')
+    }
     res.send('Login user')
 }
 
